08212beaf87cc57b29e7893cb4c51a47329dfd56
K-Mike <karchevskiy.michael@gmail.com>
2016-11-01 10:58:44 +0700

    Added CryptographerExceptions

--- app/src/main/java/com/streamdata/apps/cryptochat/cryptography/Cryptographer.java
+++ app/src/main/java/com/streamdata/apps/cryptochat/cryptography/Cryptographer.java
@@ -1,5 +1,6 @@
 package com.streamdata.apps.cryptochat.cryptography;
 
+
 public interface Cryptographer {
     String encrypt(String output) throws CryptographerException;
     String decrypt(String input) throws CryptographerException;
--- app/src/main/java/com/streamdata/apps/cryptochat/cryptography/CryptographerFactory.java
+++ app/src/main/java/com/streamdata/apps/cryptochat/cryptography/CryptographerFactory.java
@@ -2,6 +2,7 @@ package com.streamdata.apps.cryptochat.cryptography;
 
 import com.streamdata.apps.cryptochat.protocol.Protocol;
 
+
 public interface CryptographerFactory {
     Cryptographer create(byte[] blob) throws CryptographerException;
     Cryptographer create(Protocol protocol) throws CryptographerException;
