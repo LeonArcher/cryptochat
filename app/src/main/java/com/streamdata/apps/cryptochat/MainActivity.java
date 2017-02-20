fb035eb37c24214705362a52d2d4a364bfd981ca
K-Mike <karchevskiy.michael@gmail.com>
2017-02-20 16:54:20 +0700

    Create QR scanner

--- app/src/main/java/com/streamdata/apps/cryptochat/MainActivity.java
+++ app/src/main/java/com/streamdata/apps/cryptochat/MainActivity.java
@@ -1,18 +1,59 @@
 package com.streamdata.apps.cryptochat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.streamdata.apps.cryptochat.zxing.IntentIntegrator;
+import com.streamdata.apps.cryptochat.zxing.IntentResult;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, MessageListActivity.class);
+        setContentView(R.layout.qr_code_scanner);
 
-        startActivity(intent);
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId()==R.id.scan_button){
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (scanningResult != null) {
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+
+            formatTxt.setText("FORMAT: " + scanFormat);
+            contentTxt.setText("CONTENT: " + scanContent);
+        } else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
