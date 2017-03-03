29bad68d44bae588a695fce1cc73e2d09c26206a
Leon Archer <leon_archer@mail.ru>
2017-03-03 12:25:35 +0700

    Static nested class modified to non-static.  Wrong error message bug corrected (onActivityResult gets called even if ActivityNotFoundException is thrown).

--- app/src/main/java/com/streamdata/apps/cryptochat/QrCodeActivity.java
+++ app/src/main/java/com/streamdata/apps/cryptochat/QrCodeActivity.java
@@ -9,13 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.lang.ref.WeakReference;
-
 public class QrCodeActivity extends AppCompatActivity {
 
     public static final int QR_REQUEST_CODE = 0;
 
     private TextView qrCodeContentsView;
+    private boolean isQrAppInstalled = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +24,7 @@ public class QrCodeActivity extends AppCompatActivity {
         Button readQrCodeButton = (Button) findViewById(R.id.readQrCodeButton);
         qrCodeContentsView = (TextView) findViewById(R.id.qrCodeContentsView);
 
-        readQrCodeButton.setOnClickListener(new ReadQrCodeListener(this));
+        readQrCodeButton.setOnClickListener(new ReadQrCodeListener());
     }
 
     @Override
@@ -39,51 +38,41 @@ public class QrCodeActivity extends AppCompatActivity {
                 qrCodeContentsView.setText(String.format("Scanned QR code contents: %s", contents));
             }
 
-            else if (resultCode == RESULT_CANCELED) {
+            else if ((resultCode == RESULT_CANCELED) && isQrAppInstalled) {
                 qrCodeContentsView.setText("QR code scan operation canceled.");
             }
         }
     }
 
-    private static class ReadQrCodeListener implements View.OnClickListener {
+    private class ReadQrCodeListener implements View.OnClickListener {
         /***
          * Listener for QR code scanning procedure
          * (Relies upon installed QRDroid app)
          */
 
-        private final WeakReference<QrCodeActivity> parentActivityReference;
-
-        public ReadQrCodeListener(QrCodeActivity parent) {
-            parentActivityReference = new WeakReference<>(parent);
-        }
-
         @Override
         public void onClick(View v) {
-            QrCodeActivity parent = parentActivityReference.get();
-
-            if (parent == null) {
-                return;
-            }
-
             // scan if QRDroid installed
             try {
+                isQrAppInstalled = true;
                 Intent intent = new Intent("la.droid.qr.scan");
-                parent.startActivityForResult(intent, QR_REQUEST_CODE);
+                startActivityForResult(intent, QR_REQUEST_CODE);
 
             // else try to open Play Market for QRDroid page
             } catch (ActivityNotFoundException ex) {
 
-                parent.qrCodeContentsView.setText("QR code scanner is not installed. Please install QR code scanner first");
+                isQrAppInstalled = false;
+                qrCodeContentsView.setText("QR code scanner is not installed. Please install QR code scanner first");
 
                 Uri marketUri = Uri.parse("market://details?id=la.droid.qr");
                 Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
 
                 try {
-                    parent.startActivity(marketIntent);
+                    startActivity(marketIntent);
 
                 // if no Play Market installed, just write info to text view
                 } catch (ActivityNotFoundException ex2) {
-                    parent.qrCodeContentsView.setText("QR code scanner is not installed. Please install QR code scanner first");
+                    qrCodeContentsView.setText("QR code scanner is not installed. Please install QR code scanner first");
                 }
             }
         }
