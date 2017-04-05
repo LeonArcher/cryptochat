326214f6ced0ddc500ad1fd3255620e8fb32eb4e
K-Mike <karchevskiy.michael@gmail.com>
2017-04-05 19:16:58 +0700

    Added bind and unbind.

--- app/src/main/java/com/streamdata/apps/cryptochat/MainActivity.java
+++ app/src/main/java/com/streamdata/apps/cryptochat/MainActivity.java
@@ -1,20 +1,57 @@
 package com.streamdata.apps.cryptochat;
 
 import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.streamdata.apps.cryptochat.services.DateService;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean bound = false;
+    ServiceConnection serviceConnection;
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, DateService.class);
+        intent = new Intent(this, DateService.class);
+
+        serviceConnection = new ServiceConnection() {
+
+            public void onServiceConnected(ComponentName name, IBinder binder) {
+                Log.i("DateService", "MainActivity onServiceConnected");
+                bound = true;
+            }
+
+            public void onServiceDisconnected(ComponentName name) {
+                Log.i("DateService", "MainActivity onServiceDisconnected");
+                bound = false;
+            }
+        };
+
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+
         startService(intent);
 
+    }
+
+    @Override
+    protected void onPause() {
+        if (bound) {
+            Log.i("DateService", "MainActivity unbindService");
+            unbindService(serviceConnection);
+            bound = false;
+        }
+
+        super.onPause();
     }
 }
