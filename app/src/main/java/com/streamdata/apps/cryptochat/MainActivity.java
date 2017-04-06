e606b4e03469b5c3368a2b6474789cc975ba2bb1
K-Mike <karchevskiy.michael@gmail.com>
2017-04-06 19:29:41 +0700

    Added button.

--- app/src/main/java/com/streamdata/apps/cryptochat/MainActivity.java
+++ app/src/main/java/com/streamdata/apps/cryptochat/MainActivity.java
@@ -1,47 +1,36 @@
 package com.streamdata.apps.cryptochat;
 
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.streamdata.apps.cryptochat.services.DateService;
 
 public class MainActivity extends AppCompatActivity {
 
     boolean bound = false;
-    ServiceConnection serviceConnection;
-    Intent intent;
+    boolean isStarted = false;
+    DateService myService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        intent = new Intent(this, DateService.class);
-
-        serviceConnection = new ServiceConnection() {
-
-            public void onServiceConnected(ComponentName name, IBinder binder) {
-                Log.i("DateService", "MainActivity onServiceConnected");
-                bound = true;
-            }
-
-            public void onServiceDisconnected(ComponentName name) {
-                Log.i("DateService", "MainActivity onServiceDisconnected");
-                bound = false;
-            }
-        };
-
-        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
-
+        Intent intent = new Intent(this, DateService.class);
+        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
         startService(intent);
-
     }
 
     @Override
@@ -54,4 +43,32 @@ public class MainActivity extends AppCompatActivity {
 
         super.onPause();
     }
+
+    public void selfDestruct(View view) {
+        TextView textView = (TextView)findViewById(R.id.myTextView);
+
+        if (bound) {
+            textView.setText(myService.getDate());
+        } else {
+            textView.setText("Cannot bind DateService");
+        }
+    }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+
+        public void onServiceConnected (ComponentName name, IBinder binder){
+            Log.i("DateService", "MainActivity onServiceConnected");
+            bound = true;
+
+            DateService.LocalBinder mLocalBinder = (DateService.LocalBinder) binder;
+            myService = mLocalBinder.getService();
+
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            Log.i("DateService", "MainActivity onServiceDisconnected");
+            bound = false;
+        }
+    };
+
 }
